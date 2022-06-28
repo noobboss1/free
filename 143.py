e8fd80d9c74b78e7ba164e7672bda5e639d316bd
Mamun Ahmed Fahim <83030337+noobboss1@users.noreply.github.com>
2022-06-29 01:19:14 +0600

    Update 143.py

--- 143.py
+++ 143.py
@@ -1,2 +1,26 @@
 from XFORD import login
+import os
+import platform
+import webbrowser
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/XFORD-DATA')
+except:pass
+try:os.system('mkdir /sdcard/XFORD-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/XFORD-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/XFORD-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(P))
+	exit()
+
 login()
